464e0632714e1f29f6242d81a27ed35a94bb29ca
rifat <rifatalam240@gmail.com>
2025-06-08 23:21:48 +0600

    delete api done

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-require("dotenv").config();
-console.log("DB USER:", process.env.DB_USER);
-console.log("DB PASS:", process.env.DB_PASS);
-
 const express = require("express");
-
 const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
+require("dotenv").config();
+// // console.log("DB USER:", process.env.DB_USER);
+// // console.log("DB PASS:", process.env.DB_PASS);
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+// const express = require("express");
+
+// const cors = require("cors");
+
+// const app = express();
+// const port = process.env.PORT || 3000;
+
+// app.use(cors());
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.at4rpkk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -32,6 +40,42 @@ async function run() {
       const result = await servicecollection.find().limit(6).toArray();
       res.send(result);
     });
+
+    //nijer service dekhar jonno//
+    app.get("/userservice", async (req, res) => {
+      const email = req.query.email;
+      const query = { providerEmail: email };
+      const result = await servicecollection.find(query).toArray();
+      res.send(result);
+    });
+
+    //client side theke add korar jonno//
+
+    app.post("/addservice", async (req, res) => {
+      const data = req.body;
+      const result = await servicecollection.insertOne(data);
+      res.send(result);
+    });
+    //update er jonno
+    app.put("updateservice/:id", async (req, res) => {
+      const id = req.params.id;
+      const updated = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updatedoc = { $set: updated };
+      const result = await servicecollection.updateOne(filter, updatedoc);
+      res.send(result);
+    });
+
+    //delete er jonno
+
+    app.delete("/deleteservice/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await servicecollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
