b3e9809f6d9776a67e5e33933d1a888e17973ec5
rifat <rifatalam240@gmail.com>
2025-06-30 11:11:30 -0700

    initial commit from private repo

--- index.js
+++ index.js
@@ -278,6 +278,6 @@ async function run() {
 }
 run().catch(console.dir);
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is Running on port ${port}`);
 });
 
